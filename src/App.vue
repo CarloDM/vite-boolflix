b8dbefc06be2606ae35f696c440ed8150c9f54f4
carloDM <carlo.de.mauro.work@gmail.com>
2023-05-04 11:35:09 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import {store} from './data/store';
+import Header from './components/header.vue';
+import Main from './components/main.vue';
+export default {
+  name: 'app',
+  components: {
+    Header,
+    Main,
+  },
+
+  data(){
+    return {
+      store,
+    }
+  }
+}
+</script>
+
+<template>
+  
+</template>
+
+<style>
+
+</style>
