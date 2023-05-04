5cec5f8bc6a39626b50c751caf08107b210131e5
carloDM <carlo.de.mauro.work@gmail.com>
2023-05-04 19:07:37 +0200

    cleaning

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Titolo Originale
 Lingua
 Voto
 
-Milestone 2:
+Milestone 2: --(api fornisce solo lingua originale?)--
 Trasformiamo la stringa statica della lingua in una vera e propria bandiera della nazione corrispondente, gestendo il caso in cui non abbiamo la bandiera della nazione ritornata dall’API (le flag non ci sono in FontAwesome).
 
 Allarghiamo poi la ricerca anche alle serie tv. Con la stessa azione di ricerca dovremo prendere sia i film che corrispondono alla query, sia le serie tv, stando attenti ad avere alla fine dei valori simili (le serie e i film hanno campi nel JSON di risposta diversi, simili ma non sempre identici)
--- src/data/store.js
+++ src/data/store.js
@@ -2,22 +2,23 @@ import {reactive} from 'vue';
 
 export const store = reactive ({
   apiUrl : 'https://api.themoviedb.org/3/',
+  apiKey : 'api_key=d321c7f6f9c8f4981d9b27438164a67b',
+
   daySearch : 'https://api.themoviedb.org/3/trending/movie/day?',
   daySearchTv : 'https://api.themoviedb.org/3/trending/tv/day?',
-  apiKey : 'api_key=d321c7f6f9c8f4981d9b27438164a67b',
 
   searchMovie:'https://api.themoviedb.org/3/search/movie?',
   searchTv:'https://api.themoviedb.org/3/search/tv?',
   searchWord:'&query=',
-  page:'&page=',
-  pageNumb: '1',
+
   getTraslationsUrl : 'https://api.themoviedb.org/3/movie/{movie_id}/translations?api_key=<<api_key>>',
 
-  traslation:'translations?',
   lang:'&language=it-en',
   selectedLang: 'it' ,
+
   films:[],
   tv:[],
+
   inputSearch: '',
   research :'rest'
 
